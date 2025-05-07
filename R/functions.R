342f4298097c86286d59c574d85997b34337066f
Aylin Seren Güller <aylngllr@gmail.com>
2025-05-07 16:17:25 +0200

    cleaned

--- R/functions.R
+++ R/functions.R
@@ -68,3 +68,19 @@ prepare_dates <- function(data, column) {
     )
   return(prepared_dates)
 }
+
+
+
+#' Clean and prepare the CGM data
+#'
+#' @param data the cgm dataset
+#'
+#' @returns a cleaner data
+#'
+clean_cgm <- function(data) {
+  cleaned <- data |>
+    get_participant_id() |>
+    prepare_dates(device_timestamp) |>
+    dplyr::rename(glucose = historic_glucose_mmol_l)
+  return(cleaned)
+}
